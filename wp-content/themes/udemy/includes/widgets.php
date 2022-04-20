970d5b4f88432273dc2b091665a91e06cc986c20
Matt Jennings <matt.jennings@onecare.com>
2022-04-20 09:27:03 -0700

    added example widget

--- wp-content/themes/udemy/includes/widgets.php
+++ wp-content/themes/udemy/includes/widgets.php
@@ -5,5 +5,8 @@ function ju_widgets() {
         'name'          => __( 'My First Theme Sidebar', 'udemy' ),
         'id'            => 'ju_sidebar',
         'description'   => __('Sidebar for the theme Udemy', 'udemy'),
+        'before_widget' => '<div id="%1$s" class="widget clearfix %2$s">',
+        'before_title'  => '<h4>',
+        'after_title'   => '</h4>',
     ));
 }
